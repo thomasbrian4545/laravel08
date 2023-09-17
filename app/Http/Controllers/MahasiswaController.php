cc94de69c33107833ef0827441bd90fb719f0f5e
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-17 15:10:24 +0700

    Method insert()

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -7,5 +7,18 @@ use Illuminate\Support\Facades\DB;
 
 class MahasiswaController extends Controller
 {
-    //
+    public function insert()
+    {
+        $result = DB::table('mahasiswas')->insert(
+            [
+                'nim' => '19003036',
+                'nama' => 'Sari Citra Lestari',
+                'tanggal_lahir' => '2001-12-31',
+                'ipk' => 3.5,
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+        );
+        dump($result);
+    }
 }
